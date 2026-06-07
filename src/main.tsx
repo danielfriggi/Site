6d2942a56ea5da5f8c055e765cbf148b318580e4
Daniel <friggidaniel@gmail.com>
2026-06-07 06:55:13 -0300

    teste

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,13 @@ import './index.css'
 import App from './App.tsx'
 import { ThemeProvider } from './context/data-theme.tsx'
 
+const redirect = sessionStorage.getItem('redirect');
+
+if (redirect) {
+  sessionStorage.removeItem('redirect');
+  window.history.replaceState(null, '', redirect);
+}
+
 createRoot(document.getElementById('root')!).render(
   <ThemeProvider>
     <StrictMode>
